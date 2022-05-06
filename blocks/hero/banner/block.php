fce8eb769427be04d3cb2fecfbd50f38fee6fdbc
Estevão Rolim <estevao.prolim@gmail.com>
2022-05-07 00:07:25 +0100

    correction hero

--- blocks/hero/banner/block.php
+++ blocks/hero/banner/block.php
@@ -5,12 +5,14 @@ function render_block_banner($attributes)
     $title = $attributes['title'];
     $action = $attributes['action'];
     $image_url = $attributes['image'];
+    $mobile_image_url = $attributes['mobile_image'];
 
     ob_start(); // Start HTML buffering
     if ($image_url) :
 ?>
         <div class="comelli-hero-banner carousel-item">
-            <img src="<?php echo $image_url; ?>" alt="">
+            <img class="d-none d-sm-block" src="<?php echo $image_url; ?>" alt="">
+            <img class="d-block d-sm-none" src="<?php echo $mobile_image_url; ?>" alt="">
             <div class="overlay position-absolute top-0 w-100 h-100 d-flex">
                 <div class="container d-flex">
                     <div class="banner-content w-100 d-flex">
